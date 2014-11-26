0b5eeb7490e44383d940337fed68fef6cdccaef6
Travis Herrick <quasimonomial@gmail.com>
2014-11-26 10:30:29 -0800

    Finished building books controller

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -2,7 +2,7 @@ module Api
   class BooksController < ApiController
 
     def index #actually might want to delete this later if there are a lot of books
-      @books = Books.all
+      @books = Book.all
       render json: @books
     end
 
@@ -16,21 +16,36 @@ module Api
     end
 
     def new
+      #dont need it, leaving it here so I can remember what these are for now
     end
     
     def edit
-
+      #editing edits that edit
     end
 
     def show
+      @book = Book.find(params[:id])
 
+      if @book
+        render :show
+      else
+        render :json ["That book doesnt' exist", status: 422]
+      end
     end
 
     def update
-
+      @book = Book.find(params[:id])
+      if @book.update_attributes(book_params)
+        render json: @book
+      else
+        render json: @book.errors.full_messages, status:
+      end
     end
 
     def destroy #definately want this to destroy books that turn out to have the same ISBN number
+      @book = Book.find(params[:id])
+      @book.destroy
+      render json: { message: 'Book Destroyed!'}
 
     end
 
