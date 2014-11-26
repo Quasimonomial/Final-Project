63007d61f974952e84fb0015ff6c7e3062b5f5a6
Travis Herrick <quasimonomial@gmail.com>
2014-11-26 14:07:04 -0800

    Added view swap function to rotuer

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -3,5 +3,11 @@ GoodThings.Routers.GoodRouter = Backbone.Router.extend({
 		this.$rootEl = options.$rootEl
 		console.log("Starting Router");
 		console.log(this.$rootEl);
-	}
+	},
+
+	_swapView: function(view){
+		this.currentView && this.currentView.remove();
+		this.currentView = view;
+		this.$rootEl.html(view.render().$el); //remember to make renders all return this
+ 	}
 });
