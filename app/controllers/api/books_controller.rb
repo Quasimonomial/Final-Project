0e3982fcc3c92c256994cd57edd5d7d6d1a4fd6e
Travis Herrick <quasimonomial@gmail.com>
2014-11-26 10:11:44 -0800

    Set up desired functions in books controller, fleshed out index and create methods, should render things to JSON

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,6 +1,39 @@
 module Api
   class BooksController < ApiController
 
+    def index #actually might want to delete this later if there are a lot of books
+      @books = Books.all
+      render json: @books
+    end
+
+    def create
+      @book = Book.new(book_params)
+      if @book.save
+        render json: @book
+      else
+        render json: @book.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def new
+    end
+    
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy #definately want this to destroy books that turn out to have the same ISBN number
+
+    end
+
     private
     def book_params
       params.require(:book).permit(:title, :author, :year, :isbn, :publisher, :description, :cover)
