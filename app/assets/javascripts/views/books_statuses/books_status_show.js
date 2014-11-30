f5757de1e69611aa5bb15cc2077dd12870265aa1
Travis Herrick <quasimonomial@gmail.com>
2014-11-29 23:18:51 -0800

    Built book status show view, set up template files, though they are blank

---
+++ app/assets/javascripts/views/books_statuses/books_status_show.js
@@ -0,0 +1,15 @@
+Goodthings.Views.BookStatusShow = Backbone.View.extend({
+	template: JST['book_statuses/show'],
+
+	initialize: function(){
+		this.listenTo(this.model, 'sync', this.render);
+	},
+
+	render: function(){
+		var content = this.template({});
+
+		this.$el.html(content);
+		
+		return this;
+	}
+});
