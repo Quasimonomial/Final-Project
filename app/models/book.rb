d4944063f42c77d909eba6a42efb90028e9610cf
Travis Herrick <quasimonomial@gmail.com>
2014-11-29 17:13:25 -0800

    Added book status associations to book and user models

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,6 +22,11 @@ class Book < ActiveRecord::Base
   #note that I dont' quite like this, author will be a model so we will have to change that string to Author Id, but that will involve creating books to create new authors in some instances so lets bear with that for a while
 
   #oh we can have a function that takes a string of an authors name, underscores it, and stores it in the database....and when pulled up from the database, we can capitalize it again?  Or enforce similar capi\talizations...yeah we'll just add the author id column later this will work
-
+  has_many(
+    :book_statuses,
+    class_name: "BookStatus",
+    foreign_key: :book_id,
+    primary_key: :id
+  )
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,14 @@ class User < ActiveRecord::Base
   
   attr_reader :password
 
-
   after_initialize :ensure_session_token
+
+  has_many(
+    :book_statuses,
+    class_name: "BookStatus",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
 
   def self.find_by_credentials email, password
     user =User.find_by_email(email)
