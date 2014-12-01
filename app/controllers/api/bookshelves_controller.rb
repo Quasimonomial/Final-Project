435b6953d1cba006ae72311a0d02aede783bd290
Travis Herrick <quasimonomial@gmail.com>
2014-11-30 22:56:20 -0800

    set up bookshelf create method in controller

--- app/controllers/api/bookshelves_controller.rb
+++ app/controllers/api/bookshelves_controller.rb
@@ -17,7 +17,7 @@ module Api
 
     def update
       @bookshelf = Bookshelf.find(params[:id])
-      if @bookshelf.update_attributes(book_params)
+      if @bookshelf.update_attributes(bookshelf_params)
         render json: @bookshelf
       else
         render json: [@bookshelf.errors.full_messages, status: 422]
@@ -25,13 +25,23 @@ module Api
     end
 
     def create
-      
+      @bookshelf = current_user.book_statuses.new(bookshelf_params)
+      if @bookshelf.save
+        render json: @bookshelf
+      else
+        render json: @books.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def destroy
       @bookshelf = Bookshelves.find(params[:id])
       @bookshelf.try(:destroy)
       render json: {message: 'Bookshelf Destroyed'}
+    end
+
+    private
+    def bookshelf_params
+      params.require(:bookshelf).permit(:title, :description)
     end
   end
 end
