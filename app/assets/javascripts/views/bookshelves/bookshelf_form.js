19bce0400fa8710e76602d5b0546f9619a11dd54
Travis Herrick <quasimonomial@gmail.com>
2014-12-01 10:11:38 -0800

    Tried to get the form to submit properly, getting an error

--- app/assets/javascripts/views/bookshelves/bookshelf_form.js
+++ app/assets/javascripts/views/bookshelves/bookshelf_form.js
@@ -1,6 +1,10 @@
 GoodThings.Views.BookshelfForm = Backbone.View.extend({
 	template: JST["bookshelves/form"],
 
+	events: {
+		"submit form": "submit"
+	},
+
 	render: function(){
 		var content = this.template({
 			bookshelf: this.model
@@ -9,5 +13,30 @@ GoodThings.Views.BookshelfForm = Backbone.View.extend({
 		this.$el.html(content);
 
 		return this;
+	},
+
+	submit: function(event){
+		console.log("submitting");
+		event.preventDefault();
+		var attrs = $(event.target).serializeJSON();
+
+		var success = function(){
+			this.collection.add(this.model, {merge: true});
+			Backbone.history.navigate("#/bookshelves", {trigger: true});
+		}.bind(this);
+
+		function errors(model, response){
+			$('.errors').empty();
+			response.responseJSON.forEach(function(el){
+				var li = $("<li></li>")
+				li.html(el);
+				$('.errors').append(li);
+			}.bind(this));
+		}
+
+		this.model.save(attrs, {
+			success: success,
+			error: errors.bind(this)
+		})
 	}
 });
