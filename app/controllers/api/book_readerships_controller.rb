f46b7e96668fe1805f70b694238f735dceec60d0
Travis Herrick <quasimonomial@gmail.com>
2014-12-01 14:00:31 -0800

    Books readership controller now has show, update, destroy, create

--- app/controllers/api/book_readerships_controller.rb
+++ app/controllers/api/book_readerships_controller.rb
@@ -2,28 +2,44 @@ module Api
   class BookReadershipsController < ApiController
     def index
       @book_readerships = current_user.book_readerships
-      render json: @book_statuses      
+      render json: @book_readerships      
     end
 
     def create
-      
+      @book_readership = current_user.book_readerships.new(book_readership_params)
+      if @book_readership.save
+        render json: @book_readership
+      else
+        render json: @book_readership.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def show
-
+      @book_readership = BookReaderships.find(params[:id])
+      if @book_readership
+        render :show
+      else
+        rende json: ["unable to find readership"], status: 403
+      end
     end
 
     def update
-
+      @book_readership = BookReadership.find(params[:id])
+      if @book_readership.update(book_readership_params)
+        render json: @book_readership
+      else
+        render json: @book_readership.errors.full_messages, status: :unprocessable_entity
     end
 
     def destroy
-
+      @book_readership = BookReadership.find(params[:id])
+      @book_readership.try(:destroy)
+      render json: {}
     end
 
     private
     def book_readership_params
-      params.require(:book_readership).permit(:user_id, :bookshelf_id, :book_id, :status)
+      params.require(:book_readership).permit(:bookshelf_id, :book_id, :status)
     end
   end
 end
