656f829a0b0ce192c76f8e0f944bda96eff2f3aa
Travis Herrick <quasimonomial@gmail.com>
2014-12-02 03:12:51 -0800

    Added a sign in validation to the API, though I can still mess with the router

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,5 +2,6 @@ module Api
   class ApiController < ApplicationController
     #probably require sign in before anyone uses this but that is not implimented yet
     #also an class level restrictions can go here
+    before_action :require_logged_in!
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,5 +27,7 @@ class ApplicationController < ActionController::Base
   end
   
   private
-
+  def require_logged_in!
+    redirect_to new_session_url unless logged_in?
+  end
 end
